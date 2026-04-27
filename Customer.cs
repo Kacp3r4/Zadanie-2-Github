78775bb70d49166231d3013b3eda61bb70c56788
Kacp3r4 <lewandowskikacper559@gmail.com>
2026-04-27 11:03:55 +0200

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abonaments
+{
+    internal class AbonamentOffer
+    {
+        public int DlugoscUmowyMiesiace { get; }
+        public decimal OplataMiesieczna { get; }
+        private double _znizkaNaAparat;
+        public double ZnizkaNaAparat
+        {
+            get
+            {
+                return _znizkaNaAparat;
+            }
+            set
+            {
+                if (value > 50)
+                {
+                    _znizkaNaAparat = 50;
+                }
+                else
+                {
+                    _znizkaNaAparat = value;
+                }
+            }
+        }
+        public AbonamentOffer(int dlugoscUmowyMiesiace, decimal oplataMiesieczna, double znizkaNaAparat)
+        {
+            DlugoscUmowyMiesiace = dlugoscUmowyMiesiace;
+            OplataMiesieczna = oplataMiesieczna;
+            ZnizkaNaAparat = znizkaNaAparat;
+        }
+        public string PobierzInformacje()
+        {
+            return $"Umowa: {DlugoscUmowyMiesiace} msc, Opłata: {OplataMiesieczna:C}, Zniżka: {ZnizkaNaAparat}%";
+        }
+    }
+}
